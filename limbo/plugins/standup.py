3e13b420de142b0947fb23a0e9baf7474aa212ac
gshivani <gshivani@ccs.neu.edu>
2015-07-09 14:33:13 -0400

    Standup

--- limbo/plugins/standup.py
+++ limbo/plugins/standup.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
-"""!standup return the 5-day forecast"""
+"""!standup returns a standup message"""
+
+import re
+
+def on_message(msg, server):
+    text = msg.get("text", "")
+    match = re.findall(r"!standup (.*)", text)
+    if not match:
+        return
+    return "Alright, It's stand up time!" 
