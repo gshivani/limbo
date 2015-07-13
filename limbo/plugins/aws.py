ac45cf0b163dfcb8775c16c4a3f7aab722063628
gshivani <gshivani@ccs.neu.edu>
2015-07-13 09:50:30 -0400

    Adding aws

--- limbo/plugins/aws.py
+++ limbo/plugins/aws.py
@@ -6,11 +6,35 @@ import re
 
 def on_message(msg, server):
     text = msg.get("text", "")
-    match = re.findall(r"!aws (.*)", text)
-    if not match:
+    instance_id = []
+    instance_tag = []
+    ec2 = boto3.resource('ec2')
+    if re.findall(r"!aws server count", text):
+        instances = ec2.instances.all()
+        for instance in instances:
+            instance_id.append(instance.id)
+        return "There are "+ str(len(instance_id)) + " servers"
+    elif re.findall(r"!aws server running count", text):
+        instances = ec2.instances.filter(
+            Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+        for instance in instances:
+            instance_id.append(instance.id)
+        return "There are " + str(len(instance_id)) + " servers running right now"
+    elif re.findall(r"!aws server count for (.*)", text):
+        #tag = match[0]
+        instances = ec2.instances.filter(
+            Filters=[{'Name': 'tag-value', 'Values': ['nroute']}])
+        for instance in instances:
+            instance_id.append(instance.id)
+        return "There are " + str(len(instance_id)) + " servers with tag" + tag
+    else:
         return
-    s3 = boto3.resource('s3')
-    name = []
-    for bucket in s3.buckets.all():
-        name.append(bucket.name)
-    return "\n".join(name)
+    
+    
+    #s3 = boto3.resource('s3')
+    #name = []
+    #for bucket in s3.buckets.all():
+        #name.append(bucket.name)
+    #n = len(name)
+    #return "There are " + str(n) + " servers running"
+    #return "\n".join(name)
