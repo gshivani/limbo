134f199c35e9b73c5826d5d9fd0097b1a72cc0cd
gshivani <gshivani@ccs.neu.edu>
2015-07-14 13:39:00 -0400

    aws updated

--- limbo/plugins/aws.py
+++ limbo/plugins/aws.py
@@ -5,6 +5,22 @@ import boto3
 import re
 from datetime import date, timedelta
 
+
+def instance_details(instance,instance_running_details,instance_not_running_details):
+    if (instance.public_dns_name == ''):
+        instance_not_running_details.append(instance.id)
+        instance_not_running_details.append(instance.public_dns_name)
+        lt = instance.launch_time
+        instance_not_running_details.append(lt.strftime('%m/%d/%Y'))
+        instance_not_running_details.append("\n")
+    else:
+        instance_running_details.append(instance.id)
+        instance_running_details.append(instance.public_dns_name)
+        lt = instance.launch_time
+        instance_running_details.append(lt.strftime('%m/%d/%Y'))
+        instance_running_details.append("\n")
+    return [instance_not_running_details, instance_running_details]
+
 def on_message(msg, server):
     text = msg.get("text", "")
     instance_id = []
@@ -32,7 +48,7 @@ def on_message(msg, server):
             for instance in instances:
                 instance_id.append(instance.id)
             return "There are "+ str(len(instance_id)) + \
-            " servers with tag " + key + ":" + value                
+            " servers with tag " + key + ":" + value
         except ValueError:
             return "You have to give me a tag - <key:value>"
     #Displays the details (instance id, instance name and launch time) for the      instance with given tag
@@ -42,20 +58,9 @@ def on_message(msg, server):
            key,value = key_value.split(":")
            instance_running_details = []
            instance_not_running_details = []
-           instances = ec2.instances.filter(Filters=[{'Name': 'tag:'+key, 'Values': [value]}])
+           instances = ec2.instances.filter(Filters=[{'Name': 'tag:'+key.title(), 'Values': [value]}])
            for instance in instances:
-               if (instance.public_dns_name == ''):
-                   instance_not_running_details.append(instance.id)
-                   instance_not_running_details.append(instance.public_dns_name)
-                   lt = instance.launch_time
-                   instance_not_running_details.append(lt.strftime('%m/%d/%Y'))
-                   instance_not_running_details.append("\n")
-               else:
-                   instance_running_details.append(instance.id)
-                   instance_running_details.append(instance.public_dns_name)
-                   lt = instance.launch_time
-                   instance_running_details.append(lt.strftime('%m/%d/%Y'))
-                   instance_running_details.append("\n")
+               instance_not_running_details, instance_running_details = instance_details(instance,instance_running_details, instance_not_running_details)    
            return "Instance Id-" + "-Instance Name-" + "-Launch Time" + "\n" + \
            "Running: " + "\n" + " ".join(instance_running_details)+ \
            "Not Running: " + "\n" + " ".join(instance_not_running_details)
@@ -71,24 +76,24 @@ def on_message(msg, server):
             for instance in instances:
                 lt = instance.launch_time
                 if (lt.date() == date.today() - timedelta(days=int(age))):
-                    if (instance.public_dns_name == ''):
-                        instance_not_running_details.append(instance.id)
-                        instance_not_running_details.append(instance.public_dns_name)
-                        lt = instance.launch_time
-                        instance_not_running_details.append(lt.strftime('%m/%d/%Y'))
-                        instance_not_running_details.append("\n")
-                    else:
-                        instance_running_details.append(instance.id)
-                        instance_running_details.append(instance.public_dns_name)
-                        lt = instance.launch_time
-                        instance_running_details.append(lt.strftime('%m/%d/%Y'))
-                        instance_running_details.append("\n")
+                    instance_not_running_details, instance_running_details = instance_details(instance, instance_running_details, instance_not_running_details )                     
             return "Instance Id-" + "-Instance Name-" + "-Launch Time" + "\n" + \
             "Running: " + "\n" + " ".join(instance_running_details) + "Not Running: " + \
-            "\n" + " ".join(instance_not_running_details)  
+            "\n" + " ".join(instance_not_running_details)
         except ValueError:
-            return "You have to give me a number like '2'"               
+            return "You have to give me a number like '2'"
+    elif re.match(r"^!aws (.*)$", text):
+        try:
+            remove_aws = text.split(' ', 1)[1]
+            pattern = re.compile("^\s+|\s*,\s*|\s+$")
+            ids = [t for t in pattern.split(remove_aws) if t]
+            instances = ec2.instances.filter(InstanceIds=ids)
+            for instance in instances:
+                instance_id.append(instance.public_dns_name)
+            return "\n".join(instance_id)
+        except ValueError:
+            return "Just give me !aws <instance_id_1, instance_id_2, ...>"
     else:
-        return 
+        return
     
     
