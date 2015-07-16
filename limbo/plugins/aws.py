c2dee42da8c00b2dce7869e43edfcfb6d8cb7d75
gshivani <gshivani@ccs.neu.edu>
2015-07-16 09:48:52 -0400

    running re-factored code

--- limbo/plugins/aws.py
+++ limbo/plugins/aws.py
@@ -6,6 +6,7 @@ import re
 from datetime import date, timedelta
 
 STATES = ['pending', 'running', 'shutting-down', 'terminated', 'stopping', 'stopped']
+ec2 = boto3.resource('ec2')
 
 def on_message(msg, server):
     text = msg.get("text", "")
@@ -17,95 +18,102 @@ def on_message(msg, server):
     state    = re.findall(r"^!aws.*state (.*)$", text)
     age      = re.findall(r"^!aws.*age days (.*)$", text)
     instance = re.findall(r"^!aws.*instance (.*)$", text)
-
+    
     if tag:
         pattern = re.compile('.*:.*')
-        if pattern.match(tag) is None:
+        if pattern.match(tag[0]) is None:
             return "ERROR: Tag must be in key:value format. Ex: (role:nroute)"
 
     if state:
-        if state not in STATES:
-            return "ERROR: State must be one of: {}".format(STATES.join(' ,'))
+        if state[0].encode("utf8") not in STATES:
+            return "ERROR: State must be one of: {}".format(", ".join(STATES))
 
     if age:
         pattern = re.compile('\d+')
-        if pattern.match(age) is None:
+        if pattern.match(age[0]) is None:
             return "ERROR: Age must be a numeric in days. Ex: (age 5)"
 
     if instance:
         pattern = re.compile('i-.*')
-        if pattern.match(instance) is None:
+        if pattern.match(instance[0]) is None:
             return "ERROR: instance format is i-23482221. Ex: (instance i-23482221)"
 
 
     # Counting servers
-    if re.match(r"^!aws count server$", text):
+    if re.match(r"^!aws count server.*$", text):
         if state:
-            _count(state=state)
+            return _count(state=state[0].encode("utf8"))
         elif tag:
-            _count(tag=tag)
+            return _count(tag=tag[0])
         else:
-            _count()
+            return _count()
 
     # Details of servers
     if re.match(r"^!aws details.*$", text):
         if tag:
-            _details(tag=tag)
+            return _details(tag=tag[0])
         elif age:
-            _details(age=age)
+            return _details(age=age[0])
         elif instance:
-            _details(instance=instance)
+            return _details(instance=instance[0])
         else:
             return
 
 
 def _count(state=None, tag=None):
-    if state:
-        instances = ec2.instances.filter(
-            Filters=[{'Name': 'instance-state-name', 'Values': [state]}])
-        response = "There are {} servers in {} state right now".format(len(instances), state)
-    else:
-        if tag:
-            key,value = tag.split(":")
+    try:
+        if state:
             instances = ec2.instances.filter(
-                Filters=[{'Name': 'tag:{}'.format(key), 'Values': [value]}])
-            response = "There are {} servers with {} tag right now".format(len(instances), tag)
+                Filters=[{'Name': 'instance-state-name', 'Values': [state]}])
+            response = "There are {} servers in {} state right now".format(len(list(instances)), state)
         else:
-            instances = ec2.instances.all()
-            response = "There are {} servers with {} tag right now".format(len(instances), tag)
+            if tag:
+                key,value = tag.split(":")
+                instances = ec2.instances.filter(
+                    Filters=[{'Name': 'tag:{}'.format(key.title()), 'Values': [value]}])
+                response = "There are {} servers with {} tag right now".format(len(list(instances)), tag)
+            else:
+                instances = ec2.instances.all()
+                response = "There are totally {} servers right now".format(len(list(instances)))
 
-    return response
+        return response
+    except ValueError:
+        return "The value of tag/state does not match with the input format."
 
 
-def _details(state=None, tag=None, age=None):
+def _details(instance=None, tag=None, age=None):
     instance_running_details = []
     instance_not_running_details = []
+    try:
+        if tag:
+            key,value = tag.split(":")
+            instances = ec2.instances.filter(Filters=[{'Name': 'tag:{}'.format(key.title()), 'Values': [value]}])
+        elif instance:
+            instances = ec2.instances.filter(InstanceIds=List(instance))
+        else:
+            instances = ec2.instances.all()
 
-    if tag:
-        key,value = tag.split(":")
-        instances = ec2.instances.filter(Filters=[{'Name': 'tag:{}'.format(key.title), 'Values': [value]}])
-    elif instance:
-        instances = ec2.instances.filter(InstanceIds=List(instance))
-    else:
-        instances = ec2.instances.all()
+        running     = [i for i in instances if i.public_dns_name]
+        not_running = [i for i in instances if not i.public_dns_name]
 
-    running     = [instance for i in instances if i.public_dns_name]
-    not_running = [instance for i in instances if not i.public_dns_name]
-
-    if age:
+        if age:
         # refilter by age
-        running     = [instance for i in running if
-                i.launch_time.date() == date.today() - timedelta(days=int(age))]
-        not_running = [instance for i in not_running if
-                i.launch_time.date() == date.today() - timedelta(days=int(age))]
+            running     = [i for i in running if
+                    i.launch_time.date() == date.today() - timedelta(days=int(age))]
+            not_running = [i for i in not_running if
+                    i.launch_time.date() == date.today() - timedelta(days=int(age))]
 
-    response =  "Instance ID--Instance Name--Launch Time\n" + \
+        response =  "Instance ID--Instance Name--Launch Time\n" + \
                 "Running:\n"
-    for instance in running:
-        response = response + "{} - {} - {}\n".format(
-                instance.id, instance.public_dns_name, instance.launch_time.strftime('%m/%d/%Y'))
-    for instance in not_running:
-        response = response + "{} - {}\n".format(
-                instance.id, instance.launch_time.strftime('%m/%d/%Y'))
+        for instance in running:
+            response = response + "{} - {} - {}\n".format(
+                    instance.id, instance.public_dns_name, instance.launch_time.strftime('%m/%d/%Y'))
+        
+        response = response + "Not Running:\n"
+        for instance in not_running:
+            response = response + "{} - {}\n".format(
+                    instance.id, instance.launch_time.strftime('%m/%d/%Y'))
 
-    return response
+        return response
+    except ValueError:
+        return "The value of tag/state/age does not match with the input format."
